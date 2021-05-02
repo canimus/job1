631eb31f22123bb42cc4b6e2eb3601d2afbaa567
Herminio Vazquez <canimus@gmail.com>
2021-05-02 14:06:50 +0200

    Use of stream and column parsing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Please remember that it is streaming data. Here is an example which estimates the count of cat1 in col1 for every 5 minutes. You need to add the code need to estimate the average. If you know the spark, it should take 5-10 minutes.
 
+```scala
 val mavgDF = timedDF
 .withWatermark("sqltimestamp", "5 seconds")
 .groupBy(window(col("sqltimestamp"), "5 minute").as("time_frame"))
@@ -9,3 +10,4 @@ count( when( col("col1") === "cat1", 1)).as("count")
 .withColumn("window_start", col("time_frame")("start").cast(TimestampType))
 .drop("time_frame")
 .orderBy("window_start")
+```
--- job1.py
+++ job1.py
@@ -1,13 +1,48 @@
-df = spark.read.csv('test.csv')
-( 
-    df
-        .withColumn('timestamp', F.from_unixtime('sqltimestamp'))
-        .drop('sqltimestamp')
-        .withWatermark('timestamp', '5 seconds')
-        .groupby(F.window(F.col('timestamp'), '5 minute'))
-        .agg(
-            F.sum(F.when(F.col('col1') == 'cat1', 1).otherwise(0)).alias('count_cat1'), 
-            F.count(F.col('col1')).alias('total')
-        )
-        .withColumn('avg_cat1', F.expr('count_cat1 / total'))
-).show(truncate=False)
+from pyspark.sql import SparkSession, Window
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+
+def main():
+    spark = (
+        SparkSession
+            .builder
+            .appName('Streaming - Python')
+            .master('local[*]')
+            .getOrCreate()
+    )
+
+    spark.sparkContext.setJobGroup('ReadSocket', 'Reading input from nc command')
+    spark.sparkContext.setLogLevel('ERROR')
+
+    df = (
+        spark.readStream.format('socket')
+        .option('host', '127.0.0.1')
+        .option('port', 9999)
+        .load()
+    )
+
+    NUMBER_OF_COLUMNS = 5
+    _c = lambda x: str(x[0])+str(x[1])
+    expected_columns = ['sqltimestamp'] + list(map(_c, product(['col'], range(NUMBER_OF_COLUMNS))))
+    transformation_1 = (
+        df
+            .withColumn('raw_columns', F.split('value', ','))
+            .drop('value')
+            .select(*[F.col('raw_columns')[k].alias(v) for k,v in enumerate(expected_columns)])
+            .withColumn('timestamp', F.from_unixtime('sqltimestamp'))
+            .drop('sqltimestamp')
+            .withWatermark('timestamp', '5 seconds')
+            .groupby(F.window(F.col('timestamp'), '5 minute'))
+            .agg(
+                F.sum(F.when(F.col('col1') == 'cat1', 1).otherwise(0)).alias('count_cat1'), 
+                F.count(F.col('col1')).alias('total')
+            )
+            .withColumn('avg_cat1', F.expr('count_cat1 / total'))
+    )
+    
+    run_query = transformation_1.writeStream.queryName('NetworkParser').format('console').outputMode('append').start()
+    run_query.awaitTermination()
+    
+
+if __name__ == "__main__":
+    main()
