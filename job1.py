9b6cbd0321d411c9a23d2d25de75d734798983d8
Herminio Vazquez <canimus@gmail.com>
2021-05-02 14:54:14 +0200

    Working aggregation

--- job1.py
+++ job1.py
@@ -24,7 +24,7 @@ def main():
 
     NUMBER_OF_COLUMNS = 5
     _c = lambda x: str(x[0])+str(x[1])
-    expected_columns = ['sqltimestamp'] + list(map(_c, product(['col'], range(NUMBER_OF_COLUMNS))))
+    expected_columns = ['sqltimestamp'] + list(map(_c, product(['col'], range(1,NUMBER_OF_COLUMNS+1))))
     transformation_1 = (
         df
             .withColumn('raw_columns', F.split('value', ','))
@@ -32,7 +32,7 @@ def main():
             .select(*[F.col('raw_columns')[k].alias(v) for k,v in enumerate(expected_columns)])
             .withColumn('timestamp', F.from_unixtime('sqltimestamp').cast('timestamp'))
             .drop('sqltimestamp')
-            .withWatermark('timestamp', '30 seconds')
+            .withWatermark('timestamp', '5 seconds')
             .groupby(F.window(F.col('timestamp'), '5 minute'))
             .agg(
                 F.sum(F.when(F.col('col1') == 'cat1', 1).otherwise(0)).alias('count_cat1'), 
@@ -41,7 +41,7 @@ def main():
             .withColumn('avg_cat1', F.expr('count_cat1 / total'))
     )
     
-    run_query = transformation_1.writeStream.queryName('NetworkParser').format('console').outputMode('append').start()
+    run_query = transformation_1.writeStream.queryName('SocketStream').format('console').option('truncate', 'false').outputMode('update').start()
     run_query.awaitTermination()
     
 
